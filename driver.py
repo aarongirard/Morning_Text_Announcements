d53b6ddda58c725a035ebc21bcc8fd88e2ab528c
Aaron Girard <aarongirard95@gmail.com>
2016-08-08 00:20:50 -0400

    changed sms handling from textbelt.com to twilio

--- driver.py
+++ driver.py
@@ -2,9 +2,10 @@ import requests
 import json
 import datetime
 import time
+from twilio.rest import TwilioRestClient
 from message import build_message
 from dad_joke import build_dad_joke
-from creds import phone_numbers
+from creds import credentials
 
 """
 Need creds.py file with
@@ -15,11 +16,18 @@ credentials = {'city_ID', 'APPID'}
 city_ID = id of city wrt open weather
 appid = api id from open weather
 """
+PHONE_NUMBERS = credentials['phone_numbers']
+TWILIO_FROM_NUMBER = credentials['twilio_phone_number']
+TWILIO_SID = credentials['twilio_sid']
+TWILIO_AUTH_TOKEN = credentials['auth_token']
 
 def initial_msg(number):
+  client = TwilioRestClient(TWILIO_SID, TWILIO_AUTH_TOKEN)
   msg = 'You\'ve signed up for morning anouncements!'
-  post = requests.post('http://textbelt.com/text',
-    data = {'number': number, 'message': msg})
+  message = client.messages.create(body=msg,to=number,
+    from_=TWILIO_FROM_NUMBER)
+  #post = requests.post('http://textbelt.com/text',
+    #data = {'number': number, 'message': msg})
 
 def main():
   while True:
@@ -27,11 +35,12 @@ def main():
     if datetime.datetime.now().hour == 6:
       message = build_message()
       message+= '  ' + build_dad_joke()
-      for num in phone_numbers:
+      for num in PHONE_NUMBERS:
+        message = client.messages.create(body=message,to=num,
+          from_=TWILIO_FROM_NUMBER)
         #url, data{}
-        post = requests.post('http://textbelt.com/text',
-         data = {'number': num, 'message':message}
-        )
+        #post = requests.post('http://textbelt.com/text',
+         #data = {'number': num, 'message':message})
         print 'sent ' + str(num) + ' at ' +  str(datetime.datetime.now().hour)
     time.sleep(3600) #sleep for 60mins
 
